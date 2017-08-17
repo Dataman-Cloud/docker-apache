ee919a21cb10baaec6cf7bad42a4a86ed552fb77
ss75710541 <75710541@qq.com>
2017-08-17 13:17:34 +0800

    fix apache conf.d/app.conf

--- httpd/run.sh
+++ httpd/run.sh
@@ -1,5 +1,10 @@
-docker run -d
+docker rm -f apache
+docker run -it \
+	--name apache \
 	-e HTTP_PORT=82 \
 	-e HTTPS_PORT=1443 \
-	-e APP_LIST=http://127.0.0.1,http:127.0.0.1:5015 \
+	-e APP_LIST=http://tomcat.default.hatest.dataman.gateway.swan.com \
+	--net host \
+	--add-host=tomcat.default.hatest.dataman.gateway.swan.com:192.168.1.214 \
+	--entrypoint=bash \
 	library/centos7-httpd-2.4.6	
